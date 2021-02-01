d68d9384f309cafc740d12f6c056505d0110371f
denys1953 <den25640@gmail.com>
2021-02-01 20:13:53 +0200

    master

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ let points = document.querySelectorAll('.div')
 let sliderP = document.querySelector('#slider-item')
 let colours = document.querySelectorAll('.color')
 let coloursMassive = ['red', 'chartreuse', 'blue', 'purple', 'yellow', 'greenyellow']
+let massive = []
+let str = ''
 let isMouseDown, cords
 let countColour = 0
 
@@ -18,31 +20,47 @@ for (let i = 0; i < points.length; i++) {
    })
 }
 let cord = sliderP.offsetLeft
-sliderP.onmousedown = (e) => {
+
+sliderP.addEventListener('touchstart', mouseStart)
+sliderP.style.marginLeft = 0 + 'px'
+function mouseStart(e) {
+   let marginLeft = sliderP.style.marginLeft
    let coords = getCoords(sliderP);
-   let shiftX = e.pageX - coords.left;
-   moveAt(e)
-   function moveAt(e) {
-      sliderP.style.marginLeft = e.pageX - cord - shiftX + 'px';
+   let shiftX = e.touches[0].screenX - coords.left;
+   massive.length = 0
+   for (let i = 0; i < marginLeft.length; i++) {
+      massive.push(marginLeft[i])
    }
-   document.onmousemove = (e) => {
-      moveAt(e);
+   massive.splice(-2, 2)
+   console.log(massive)
+   str = ''
+   for (let i = 0; i < massive.length; i++) {
+      str+=massive[i]
    }
-   sliderP.onmouseup = () => {
-      document.onmousemove = null;
-      sliderP.onmouseup = null;
+   if (Number(str) > 0) {
+      sliderP.style.marginLeft = 0
    }
-   sliderP.ondragstart = function() {
-      return false;
-   }
+   sliderP.addEventListener('touchmove', (e) => {
+      if (Number(str) <= 0) {
+         sliderP.style.marginLeft = e.touches[0].screenX - cord - shiftX + 'px'
+         console.log(str)
+      } else {
+         sliderP.style.marginLeft  = 0
+      }
+   })
+   sliderP.addEventListener('touchend', () => {
+      if (Number(str) > 0) {
+         sliderP.style.marginLeft  = 0
+      }
+   })
 }
+
 function getCoords(elem) {  
    let box = elem.getBoundingClientRect();
    return {
      left: box.left + pageXOffset
    }
 }
-
 
 
 

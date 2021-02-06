cb8e0b78f51b605f45605a94daebde2ec33c924e
denys1953 <den25640@gmail.com>
2021-02-06 19:46:52 +0200

    master

--- main.js
+++ main.js
@@ -1,27 +1,25 @@
 let points = document.querySelectorAll('.div')
 let sliderP = document.querySelector('#slider-item')
 let colours = document.querySelectorAll('.color')
-let img = document.getElementById('img').clientWidth
-// let coloursMassive = ['red', 'chartreuse', 'blue', 'purple', 'yellow', 'greenyellow']
+let img = sliderP.clientWidth + 16
+
+sliderP.addEventListener('touchstart', mouseStart)
+sliderP.style.marginLeft = 0 + 'px'
+let word = 'transition'
+let cord = sliderP.offsetLeft
 let massive = []
 let str = ''
 let isMouseDown, cords
 let countCowlour = 0
 let count = img
-console.log(img)
-
-// for (let i = 0; i < colours.length; i++) {
-//    colours[i].style.backgroundColor = coloursMassive[countColour]
-//    countColour++
-// }
 
 for (let i = 0; i < points.length; i++) {
    points[i].setAttribute('id', count - img) 
    count -= img
    points[i].addEventListener('click', (e) => {
-      sliderP.style.transition = 0.2 + 's'
+      sliderP.style.transition = 0.4 + 's'
       sliderP.style.marginLeft = points[i].id + 'px'
-      setTimeout(() => sliderP.style.transition = 0 + 's', 201)
+      setTimeout(() => sliderP.style.transition = 0 + 's', 401)
       if (points[i].classList.contains('active')) {
          for (let j = 0; j < points.length; j++) {
             points[j].classList.remove('active')
@@ -35,41 +33,28 @@ for (let i = 0; i < points.length; i++) {
       }
    })
 }
-let cord = sliderP.offsetLeft
 
-sliderP.addEventListener('touchstart', mouseStart)
-sliderP.style.marginLeft = 0 + 'px'
 function mouseStart(e) {
-   let marginLeft = sliderP.style.marginLeft
    let coords = getCoords(sliderP);
    let shiftX = e.touches[0].screenX - coords.left;
-   if (Number(str) > 0) {
-      sliderP.style.marginLeft = 0
-   }
    sliderP.addEventListener('touchmove', (e) => {
-      massive.length = 0
-      for (let i = 0; i < marginLeft.length; i++) {
-         massive.push(marginLeft[i])
-      }
-      massive.splice(-2, 2)
-      str = ''
-      for (let i = 0; i < massive.length; i++) {
-         str+=massive[i]
-      }
-      if (Number(str) <= 0) {
-         sliderP.style.marginLeft = e.touches[0].screenX - cord - shiftX + 'px'
+      str = sliderP.getAttribute('style')
+      if (str.includes(word)) {
+         str = str.slice(13, -31)
       } else {
-         sliderP.style.marginLeft  = 0
-         console.log(3)
-         e.preventDefault()
+         str = str.slice(13, -3)
       }
-   })
-   sliderP.addEventListener('touchend', () => {
       if (Number(str) > 0) {
-         sliderP.style.marginLeft  = 0
+         // sliderP.style.marginLeft  = 0
+
+         sliderP.style.marginLeft = e.touches[0].screenX - cord - shiftX + 'px'
+      } else if (Number(str) <= 0) {
+         sliderP.style.marginLeft = e.touches[0].screenX - cord - shiftX + 'px'
       }
    })
 }
+
+
 
 function getCoords(elem) {  
    let box = elem.getBoundingClientRect();
